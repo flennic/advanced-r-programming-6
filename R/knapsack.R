c3a8b00e0f882d506b45b40e5d62e12051a98f46
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-10-09 16:05:38 +0200

    Add unit-tests

--- R/knapsack.R
+++ R/knapsack.R
@@ -1,4 +1,4 @@
-library(binaryLogic)
+# library(binaryLogic)
 
 #' combination_object_output
 #'
@@ -69,14 +69,14 @@ knapsack_input_validation = function(df, W) {
   if (!is.numeric(W)) stop("W must be numeric.")
 }
 
-set.seed(42)
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000)
-  )
-
-print(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500))
-print(brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500))
-print(brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000))
-print(brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000))
+# set.seed(42)
+# n <- 2000
+# knapsack_objects <-
+#   data.frame(
+#     w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000)
+#   )
+#
+# print(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500))
+# print(brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500))
+# print(brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000))
+# print(brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000))

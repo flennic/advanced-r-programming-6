789b90fc79cccbc6e223fbb14b6d030ffb759512
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-10-10 14:04:09 +0200

    Fix typo

--- R/knapsack.R
+++ R/knapsack.R
@@ -38,7 +38,7 @@ brute_force_knapsack = function(x, W) {
   # All combinations of items as binary. If more than 32 objects, it breaks
   bin_rep = sapply(c(1:2^limit-1), function(x) { as.binary(x, n = limit) })
 
-  # Get a list ob combination objects containign v, w and s
+  # Get a list ob combination objects containing v, w and s
   combination_objects = apply(bin_rep, 2, get_combination_object)
 
   # Convert list of lists to a better usable dat.frame

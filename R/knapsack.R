3334bb8a72aa968bd28835a678e2eec34a7f95e5
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-10-14 19:35:51 +0200

    Add parallel version of brute force knapsack

--- R/knapsack.R
+++ R/knapsack.R
@@ -1,4 +1,5 @@
 #library(binaryLogic)
+#library(parallel)
 
 #' combination_object_output
 #'
@@ -18,7 +19,7 @@ combination_object_output = function(obj) {
 #'
 #' @return Returns the best combination of objects to pick, so that v is max.
 #' @export
-brute_force_knapsack = function(x, W) {
+brute_force_knapsack = function(x, W, parallel = FALSE) {
   knapsack_input_validation(x, W)
   limit = nrow(x)
 
@@ -30,14 +31,37 @@ brute_force_knapsack = function(x, W) {
     return(list(v = sum(selectedItems$v), w = sum(selectedItems$w), s = selector))
   }
 
-  # All combinations of items as binary. If more than 32 objects, it breaks
-  bin_rep = sapply(c(1:2^limit-1), function(x) { as.binary(x, n = limit) })
+  if (parallel) {
+    # Initiate cluster
+    cl = makeCluster(detectCores() - 1)
+    clusterEvalQ(cl, {
+      library(binaryLogic)
+      library(parallel)
+    })
 
-  # Get a list ob combination objects containign v, w and s. v and w will be 0 if w > W
-  combination_objects = apply(bin_rep, 2, get_combination_object)
+    # All combinations of items as binary. If more than 32 objects, it breaks
+    bin_rep = parSapply(cl, c(1:2^limit-1), function(x) { as.binary(x, n = limit) })
 
-  # Convert list of lists to a better usable dat.frame
-  combination_objects.as.df = as.data.frame(t(sapply(combination_objects, rbind)))
+    # Get a list ob combination objects containign v, w and s. v and w will be 0 if w > W
+    combination_objects = parApply(cl, bin_rep, 2, get_combination_object)
+
+    # Convert list of lists to a better usable dat.frame
+    combination_objects.as.df = as.data.frame(t(parSapply(cl, combination_objects, rbind)))
+
+    # Stop Cluster
+    stopCluster(cl)
+  }
+  else {
+    # All combinations of items as binary. If more than 32 objects, it breaks
+    bin_rep = sapply(c(1:2^limit-1), function(x) { as.binary(x, n = limit) })
+
+    # Get a list ob combination objects containign v, w and s. v and w will be 0 if w > W
+    combination_objects = apply(bin_rep, 2, get_combination_object)
+
+    # Convert list of lists to a better usable dat.frame
+    combination_objects.as.df = as.data.frame(t(sapply(combination_objects, rbind)))
+  }
+
   colnames(combination_objects.as.df) = c('v', 'w', 's')
 
   # Filter all entries that exceed the weight limit
@@ -139,14 +163,15 @@ knapsack_input_validation = function(df, W) {
   if (W < 0) stop("W msut not be negative")
 }
 
-#set.seed(42)
-#n <- 2000
-#knapsack_objects <-
-#  data.frame(
-#    w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000)
-#)
+set.seed(42)
+n <- 2000
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000)
+)
 
 #print(system.time(brute_force_knapsack(x = knapsack_objects[1:16,], W = 3500)))
+#print(system.time(brute_force_knapsack(x = knapsack_objects[1:16,], W = 3500, parallel = TRUE)))
 #print(system.time(dynamic_knapsack(x = knapsack_objects[1:500,], W = 3500)))
 #print(system.time(greedy_knapsack(x = knapsack_objects[1:1000000,], W = 3500)))
 #print(brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500))
